a78abaa7be6dfc45c49410ec6838cc20b1885944
indrnoken <61928913+indrnoken@users.noreply.github.com>
2020-03-21 12:11:47 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_praktikum3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            taxi taxi1 = new taxi();
+
+            taxi1.Drivername = "Zoro";
+            taxi1.OnDuty = true;
+            taxi1.NumPassenger = 10;
+
+
+            taxi1.TaxiInfo();
+            taxi1.dropofpassenger();
+            taxi1.pickuppassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_praktikum3
+{
+  public  class taxi
+    {
+        public string Drivername { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Nama Driver : {0}", Drivername);
+            if (OnDuty==true)
+            {
+                Console.WriteLine("On duty : Yes");
+            }
+            if (OnDuty==false)
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine(" Number of passenger : {0}", NumPassenger);
+            
+        }
+        public void pickuppassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", Drivername);
+
+        }
+        public void dropofpassenger()
+        {
+            Console.WriteLine(" {0} sudah selesai mengantar penumpang");
+        }
+
+        
+    }
+}
